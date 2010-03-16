375c87e350efb7b17d946734a5671a0d13b1e0db
Ryan Zezeski <rzezeski@gmail.com>
2010-03-15 23:05:38 -0400

    Update project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject net.progski/repl-share "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject net.progski/repl-share "0.1-SNAPSHOT"
+  :description "Share your REPL over the LAN!"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
