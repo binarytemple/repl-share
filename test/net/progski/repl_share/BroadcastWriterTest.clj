74ec709554d829738850e61a00e5542eb3bf0655
Ryan Zezeski <rzezeski@gmail.com>
2010-03-31 23:34:51 -0400

    Add more test cases to BroadcastWriterTest.

--- test/net/progski/repl_share/BroadcastWriterTest.clj
+++ test/net/progski/repl_share/BroadcastWriterTest.clj
@@ -1,18 +1,37 @@
 (ns net.progski.repl-share.BroadcastWriterTest
+  (:use [net.progski.repl-share.broadcast :only (broadcast)])
   (:use clojure.test)
   (:import net.progski.repl_share.BroadcastWriter))
 
 (declare *bw*)
+(def *broadcast-flag* (atom false))
 
+;; Helpers
+(defn buff-str []
+  (apply str @(:buff (.state *bw*))))
+
+(defn mock-broadcast [_ _]
+  (reset! *broadcast-flag* true))
+
+;; Fixtures
 (defn bw-fixture [f]
   (binding [*bw* (BroadcastWriter. "test" (java.io.StringWriter.))]
     (f)))
 
-(use-fixtures :each bw-fixture)
+(defn broadcast-fixture [f]
+  (reset! *broadcast-flag* false)
+  (binding [broadcast mock-broadcast]
+    (f)))
 
-(defn buff-str []
-  (apply str @(:buff (.state *bw*))))
+(use-fixtures :each bw-fixture broadcast-fixture)
 
-(deftest write
-  (.write *bw* (char-array "4\n[share] user=> "))
-  (is (= "4\n[share] user=> " (buff-str))))
+;; Test cases
+(deftest test-write
+  (.write *bw* (char-array "foo"))
+  (is (= "foo" (buff-str))))
+
+(deftest test-flush
+  (binding [broadcast mock-broadcast]
+    (.flush *bw*)
+    (is @*broadcast-flag*)
+    (is (empty? (buff-str)))))
